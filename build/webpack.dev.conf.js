79b8513b58e87c552eaba130b6eb46d85976fe7c
xianglei <15067180409xl@gmail.com>
2018-11-08 10:45:26 +0800

    修改构建方案

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -69,6 +69,22 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'img/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[hash:7].[ext]'
+        }
       }
     ]
   },
