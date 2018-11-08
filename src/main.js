5f1b98a4efdb0b368d1cdf6145bf704c6c5812cf
xianglei <15067180409xl@gmail.com>
2018-11-08 10:46:32 +0800

    修改页面

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import router from './router'
 import store from './store'
 import App from './App'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
+Vue.use(iView)
 
 new Vue({
   el: '#app',
